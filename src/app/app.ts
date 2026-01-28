2239935fc89f942809667ba1b9cafed2ee1329da
Joemol Joy <joemoljoy.a@gmail.com>
2026-01-28 13:24:38 -0500

    Add form fields for email, gender, subscription, and terms acceptance with validation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,34 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, NgIf],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('my-form-app');
   userName = "";
+  userEmail = "";
+  userGender = "";
+  termsAccepted = false;
+  subscription = '';
+  formSubmitted = false;
+
+  onSubmit(form: any) {
+    if (form.valid) {
+      console.log("Form Data: ", form.value);
+      this.formSubmitted = true;
+
+      form.resetForm();        // resets the form
+      this.userName = '';
+      this.userEmail = '';
+      this.userGender = '';
+      this.termsAccepted = false;
+      this.subscription = '';
+    }
+  }
 }
